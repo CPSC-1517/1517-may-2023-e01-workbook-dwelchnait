c72098c2dcf7bfd9861f6639e9e7b379ed711501
Don Welch <dwelch@nait.ca>
2023-05-10 18:40:18 -0600

    created loop example

--- GitHubPracticeSolution/GitHubPractice/Program.cs
+++ GitHubPracticeSolution/GitHubPractice/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello Don, the wonderful world of github!");
+
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"iteration number {i}");
+}
