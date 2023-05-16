f0576057b1e7966932b8e856c7cb741474690433
Don Welch <dwelch@nait.ca>
2023-05-15 18:50:29 -0600

    review constructors

--- OOPsSolution/OOPsReview/Employment.cs
+++ OOPsSolution/OOPsReview/Employment.cs
@@ -123,6 +123,71 @@ namespace OOPsReview
         //using a public or private set is a design decision
         public DateTime StartDate { get; private set; }
 
+        // Constructors
+        //
+        // the purpose of a constructor is to create an instance of your class
+        //      in a known state.
+        //does a class definition need a constructor? NO.
+        //   if a class definition does NOT have a constructor then the system
+        //   will create the instance and assign the system default value to data member
+        //   and/or auto implemented property
+        //   if you have no constructor the common phrase is "using a system constructor"
+        //
+        //IF YOU CODE A CONSTRUCTOR IN YOUR CLASS YOU ARE RESPONSIBLE FOR ANY AND ALL CONSTRUCTORS
+        //FOR THE CLASS!!!
 
+
+        //"default" constructor
+        //you can apply your own literal values for your data members and/or auto-implemented properties
+        //  that differ from the system default values
+        //why? you may have data that is validated and using the system default values would cause an
+        //  exception
+        public Employment()
+        {
+            //this constructor will be used on creating an instance using
+            //     Employment myInstance = new Employment();
+            //A practice that I personally use is to avoid referring my data members directly
+            //  specially if the property contains validation
+            Title = "Unknown";
+            Level = SupervisoryLevel.TeamMember;
+            StartDate = DateTime.Today;
+            //optionally one could set years to zero, but that is the system default
+            //  value for a numeric, therefore one does not need to assign a value
+            //  UNLESS you wish to
+
+        }
+
+        //greedy constructor
+        //a greedy constructor is one that accepts a parameter list of values to
+        //  assign to your instance data on creation of the instance
+        //generally your greedy constructor constains a parameter on the signature
+        //  for each data member you have in your class definition
+
+        //all default parameters must appear AFTER non-default parameters in your parameter list
+        //in this example, we will use Years as an default parameter
+        public Employment(string title, SupervisoryLevel level,
+                            DateTime startdate, double years = 0.0)
+        {
+            Title = title;
+            Level = level;
+            Years = years;
+            //this example to demonstrating where you can place validation for
+            //  properties have are auto-implemented
+            //validate start data must not exist in the future
+            //validation can be done anywher in your class
+            //since the property is auto-implemented AND has a privat set
+            //      validation can be done in the constructor OR a method
+            //      that alters the property value
+            //IF the validation is done in the property, IT WOULD NOT be an
+            //      auto-implemented property BUT a fully-implemented property
+            //.Today has a time of 00:00:000 AM
+            //.Now has a specific time of day at execution 18:47:256 PM
+            //by using the .Today.AddDays(1) you cover all times on a specific date
+            if (startdate >= DateTime.Today.AddDays(1))
+            {
+                throw new ArgumentException($"The start date {startdate} is in the future");
+            }
+            StartDate = startdate;
+        }
     }
 }
--- OOPsSolution/OOPsReview/Utilities.cs
+++ OOPsSolution/OOPsReview/Utilities.cs
@@ -6,7 +6,60 @@ using System.Threading.Tasks;
 
 namespace OOPsReview
 {
-    internal class Utilities
+    public static class Utilities
     {
+        //static classes are NOT instantiated by the outside user (developer/code)
+        //static class items are referenced using:  classname.xxxx
+        //methods within this class have the keyword static in their signature
+        //static classes are shared between all outside users at the same time
+        //DO NOT consider saving data within s static class BECAUSE you cannot be
+        //  certain it will be there when your use the class again
+        //consider placing GENERIC re-usable methods with a static class
+
+        //sample of a generic method: numeric is a zero or positive value
+        public static bool IsZeroOrPositive(double value)
+        {
+            //a structure method (apply to loops, etc) will have a single entry point
+            //  and a single exit point
+            //in this course you WILL AVOID where possible multiple returns from a method
+            //in this course you WILL AVOID using a break to exit a loop structure or if structure
+            bool valid = true;
+            if (value < 0.0)
+            {
+                valid = false;
+            }
+            return valid;
+        }
+
+        //overload the IsZeroOrPositive so that it uses integers or decimals
+
+        public static bool IsZeroOrPositive(int value)
+        {
+            //a structure method (apply to loops, etc) will have a single entry point
+            //  and a single exit point
+            //in this course you WILL AVOID where possible multiple returns from a method
+            //in this course you WILL AVOID using a break to exit a loop structure or if structure
+            bool valid = true;
+            if (value < 0)
+            {
+                valid = false;
+            }
+            return valid;
+        }
+
+        public static bool IsZeroOrPositive(decimal value)
+        {
+            //a structure method (apply to loops, etc) will have a single entry point
+            //  and a single exit point
+            //in this course you WILL AVOID where possible multiple returns from a method
+            //in this course you WILL AVOID using a break to exit a loop structure or if structure
+            bool valid = true;
+            if (value < 0.0m)
+            {
+                valid = false;
+            }
+            return valid;
+        }
+
     }
 }
