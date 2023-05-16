62cf959e835b0d408249c27b84aa7f433831303e
Don Welch <dwelch@nait.ca>
2023-05-15 18:08:01 -0600

    complete properties review, demonstrate settig up a common utility static class with overloaded methods

--- OOPsSolution/OOPsReview/Employment.cs
+++ OOPsSolution/OOPsReview/Employment.cs
@@ -57,7 +57,7 @@ namespace OOPsReview
             //referred to as the mutator
             //it is within the set that the validation of the data
             //  is done to determine if the data is acceptable
-
+            //by default set is public
             set 
             {
                 if (string.IsNullOrWhiteSpace(value))
@@ -70,5 +70,59 @@ namespace OOPsReview
                 //}
             }
         }
+
+        public  SupervisoryLevel Level
+        {
+            get { return _Level; }
+            private set
+            {
+                //a private set means that the property can only be set
+                //  by code within the class definition
+                //an advantage of doing this is increaseing security on the
+                //  field as any change is under the control of the class definition
+                //
+                //validate tha the value given as an enum is actually defined
+                //
+                //a user of this class could send in an integer value that was
+                //  type casted as this enum datatype BUT have a non-defined value
+                //
+                //to test for a defined enum value use Enum.IsDefined(typeof(xxxx), value)
+                //  where xxxx is the name of the enum datatype
+                if (!Enum.IsDefined(typeof(SupervisoryLevel), value)) 
+                {
+                    throw new ArgumentException($"Supervisory level is invalid: {value}");
+                }
+                _Level = value;
+            }
+        }
+
+        //validate the years of service in the employee position as a positive value.
+
+        public double Years
+        {
+            get { return _Years; } //used on the right side of an assignment statement or in an expression
+            set                   //used on the left side of an assignment statement
+            {
+                //if ( value < 0 ) //using a Utilities generic method to do this test
+                if (!Utilities.IsZeroOrPositive(value))
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+                _Years = value;
+            }
+        }
+
+        //this property is an example of an auto-implemented property
+        //there is no validation within the property
+        //there is NO private data member for this property
+        //the system will generate an internal storage area for the data
+        //      and handle the setting and getting from that storage area
+        //the private set means that the property will only be able to be
+        //      set via a constructor or method.
+        //auto-implemented properties can have either a public or private set
+        //using a public or private set is a design decision
+        public DateTime StartDate { get; private set; }
+
+
     }
 }
