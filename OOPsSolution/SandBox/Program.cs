a3e2d39ad5e1d9e0f76ca04be3ba77cdb38e43d0
Don Welch <dwelch@nait.ca>
2023-05-24 17:46:09 -0600

    commit introduction to the record datatype

---
+++ OOPsSolution/SandBox/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOPsReview;
+
+Console.WriteLine("Hello, World!");
+
+Residence myHome = new Residence(123, "Maple St.", "Edmonton", "AB", "T6Y7U8");
+Console.WriteLine(myHome.ToString());
+
+//can i change a value in the record instance? NO!
+//myHome.Number = 321;
+
+//to alter a value in the record instance you MUST create a new instance
+myHome = new Residence(321, "Maple St.", "Edmonton", "AB", "T6Y7U8");
+Console.WriteLine(myHome.ToString());
