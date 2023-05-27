df454d8d92eaac834e46ab17cdf3065e372a94ad
Don Welch <dwelch@nait.ca>
2023-05-26 18:03:53 -0600

    Complete unit test for Default and Greedy constructors.

--- OOPsSolution/OOPsReview/Person.cs
+++ OOPsSolution/OOPsReview/Person.cs
@@ -8,12 +8,33 @@ namespace OOPsReview
 {
     public class Person
     {
-        public string FirstName;
-        public string LastName;
-        public Person(string firstname, string lastname)
+        private string _FirstName;
+        private string _LastName;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Residence Address { get; set; }
+        public List<Employment> EmploymentPositions { get; set; } = new List<Employment>();
+
+        public Person(string firstname, string lastname, Residence address, List<Employment> employmentpositions)
         {
             FirstName = firstname;
             LastName = lastname;
+            Address = address;
+            if (employmentpositions != null)
+            {
+                EmploymentPositions = employmentpositions;  //store the supplied list of employments
+            }
+            //else
+            //{
+            //    EmploymentPositions= new List<Employment>();  //create an empty instance of the list
+            //}
+        }
+
+        public Person()
+        {
+            //EmploymentPositions= new List<Employment>();  //create an empty instance of the list}
+
         }
     }
 }
--- OOPsSolution/TDDUnitTestDemo/Person_Should.cs
+++ OOPsSolution/TDDUnitTestDemo/Person_Should.cs
@@ -8,20 +8,40 @@ namespace TDDUnitTestDemo
         //Attribute title
         //  Fact which does one test and is usually setup and coded within the test
         //  Theory which allow for multiple test data stream applied to the same test
+        [Fact]
+        public void Create_an_Instance_Using_Default_Constructor()
+        {
+            //Arrange (setup)
+           
+            
+            //Act (execution) (sut subject under test)
+            Person sut = new Person();
+
+            //Assert (testing of the action)
+            sut.FirstName.Should().BeNull();
+            sut.LastName.Should().BeNull();
+            sut.Address.Should().BeNull();
+            sut.EmploymentPositions.Count().Should().Be(0);
+        }
 
         [Fact]
-        public void Create_an_Instance_With_First_And_Last_Name()
+        public void Create_an_Instance_With_First_And_Last_Name_Residence_With_NO__List_Of_Employment()
         {
             //Arrange (setup)
             string firstname = "Don";
             string lastname = "Welch";
+            Residence address = new Residence(123, "Maple St.", "Edmonton", "AB", "T6Y7U8");
+            string expectedaddress = "123,Maple St.,Edmonton,AB,T6Y7U8";
+
 
             //Act (execution) (sut subject under test)
-            Person sut = new Person(firstname, lastname);
+            Person sut = new Person(firstname, lastname, address, null);
 
             //Assert (testing of the action)
             sut.FirstName.Should().Be(firstname);
             sut.LastName.Should().Be(lastname);
+            sut.Address.ToString().Should().Be(expectedaddress);
+            sut.EmploymentPositions.Count().Should().Be(0);
         }
     }
 }
